4433bc114225a382f680910dfe12d19f5e48695c
VinzentM <vinzentmoesch@hanovernorwichschools.org>
2016-12-13 13:30:31 -0500

    Updated from Brython Server: 12/13/2016 1:30:31 PM

--- spaceflight2d.py
+++ spaceflight2d.py
@@ -105,8 +105,8 @@ class astroid(Sprite):
             print(self.angle1)
             self.angle2 = math.atan((self.avy)/(self.avx))
             
-            self.vx = (math.sqrt(self.vx**2+self.vy**2)*math.cos(self.angle2-self.angle1)
-            self.vy= (math.sqrt(self.vx**2+self.vy**2)*math.sin(self.angle2-self.angle1)
+            self.vx = (math.sqrt(self.vx**2+self.vy**2)*math.cos(self.angle2-self.angle1))
+            self.vy = (math.sqrt(self.vx**2+self.vy**2)*math.sin(self.angle2-self.angle1))
             
             print(self.angle2)
             self.avy = 0
